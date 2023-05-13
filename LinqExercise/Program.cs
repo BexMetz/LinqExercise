54155be826e66098e31ed936d47d3416e8c3b243
Bex <bexmetzger@gmail.com>
2023-05-13 07:11:08 -0700

    Exercise 3 completed

--- LinqExercise/Program.cs
+++ LinqExercise/Program.cs
@@ -21,30 +21,86 @@ namespace LinqExercise
              */
 
             //TODO: Print the Sum of numbers
+            int numSum = numbers.Sum();
+            Console.WriteLine($"Numbers sum:");
+            Console.WriteLine(numSum);
+            Console.WriteLine();
 
             //TODO: Print the Average of numbers
+            double numAve = numbers.Average();
+            Console.WriteLine($"Numbers average:");
+            Console.WriteLine(numAve);
+            Console.WriteLine();
 
             //TODO: Order numbers in ascending order and print to the console
+            var numAscend = numbers.OrderBy(n => n);
+            Console.WriteLine($"Numbers ascending:");
+            foreach (int n in numAscend) { Console.WriteLine(n); }
+            Console.WriteLine();
 
             //TODO: Order numbers in decsending order and print to the console
+            var numDescend = numbers.OrderByDescending(n => n);
+            Console.WriteLine($"Numbers descending:");
+            foreach (int n in numDescend) { Console.WriteLine(n); }
+            Console.WriteLine();
 
             //TODO: Print to the console only the numbers greater than 6
+            var greaterSix = numbers.Where(n => n > 6);
+            Console.WriteLine($"Numbers greater than 6:");
+            foreach(int n in greaterSix) { Console.WriteLine(n); }
+            Console.WriteLine();
 
             //TODO: Order numbers in any order (acsending or desc) but only print 4 of them **foreach loop only!**
 
-            //TODO: Change the value at index 4 to your age, then print the numbers in decsending order
+            var onlyFour = numbers.OrderByDescending(n => n).Where(n => n % 2 == 0 && n != 0 );
+            Console.WriteLine($"Only 4 of the descending numbers:");
+            foreach(int n in onlyFour) { Console.WriteLine(n); }
+            Console.WriteLine();
 
+            //TODO: Change the value at index 4 to your age, then print the numbers in decsending order
+            numbers.SetValue(32, 4);
+            Console.WriteLine($"Index 4 changed to age:");
+            foreach (int n in numDescend ) { Console.WriteLine(n); }
+            Console.WriteLine();
+
+
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine();
             // List of employees ****Do not remove this****
             var employees = CreateEmployees();
 
-            //TODO: Print all the employees' FullName properties to the console only if their FirstName starts with a C OR an S and order this in ascending order by FirstName.
+            //TODO: Print all the employees' FullName properties to the console only if their
+            //FirstName starts with a C OR an S and order this in ascending order by FirstName.
+            Console.WriteLine("Employees with a first name beginning with C or S:");
+            employees.Where(e => e.FirstName[0] == 'C' || e.FirstName[0] == 'S')
+                .OrderBy(e => e.FirstName)
+                .ToList()
+                .ForEach(e => Console.WriteLine(e.FullName));
+            Console.WriteLine();
 
-            //TODO: Print all the employees' FullName and Age who are over the age 26 to the console and order this by Age first and then by FirstName in the same result.
+            //TODO: Print all the employees' FullName and Age who are over the age 26 to the
+            //console and order this by Age first and then by FirstName in the same result.
+            Console.WriteLine("Employees over 26 (ordered by age then name):");
+            employees.Where(e => e.Age > 26).OrderBy(e => e.Age).ThenBy(e => e.FirstName)
+                .ToList().ForEach(e => Console.WriteLine($"{e.Age}  {e.FullName}"));
+            Console.WriteLine();
 
-            //TODO: Print the Sum and then the Average of the employees' YearsOfExperience if their YOE is less than or equal to 10 AND Age is greater than 35.
+            //TODO: Print the Sum and then the Average of the employees' YearsOfExperience if
+            //their YOE is less than or equal to 10 AND Age is greater than 35.
+            Console.WriteLine("Employees over 35 with 10 or less years experience:");
+            int sumOfYOE = employees.Where(e => e.Age > 35 && e.YearsOfExperience <= 10).Sum(e => e.YearsOfExperience);
+            Console.WriteLine($"Total years of experience: {sumOfYOE}");
+            double aveOfYOE = employees.Where(e => e.Age > 35 && e.YearsOfExperience <= 10).Average(e => e.YearsOfExperience);
+            Console.WriteLine($"Average years of experience: {aveOfYOE}");
+            Console.WriteLine();
 
             //TODO: Add an employee to the end of the list without using employees.Add()
-
+            Console.WriteLine("Original Employee List:");
+            foreach (var e in employees) { Console.WriteLine($"{e.FullName} / Age:{e.Age} / YOE:{e.YearsOfExperience}"); }
+            Console.WriteLine();
+            Console.WriteLine("Appended Employee List:");
+            employees = employees.Append(new Employee("Bex", "Metz", 32, 2)).ToList();
+            foreach (var e in employees) { Console.WriteLine($"{e.FullName} / Age:{e.Age} / YOE:{e.YearsOfExperience}"); }
 
             Console.WriteLine();
 
@@ -59,7 +115,7 @@ namespace LinqExercise
             employees.Add(new Employee("Steven", "Bustamento", 56, 5));
             employees.Add(new Employee("Micheal", "Doyle", 36, 8));
             employees.Add(new Employee("Daniel", "Walsh", 72, 22));
-            employees.Add(new Employee("Jill", "Valentine", 32, 43));
+            employees.Add(new Employee("Jill", "Valentine", 32, 4));
             employees.Add(new Employee("Yusuke", "Urameshi", 14, 1));
             employees.Add(new Employee("Big", "Boss", 23, 14));
             employees.Add(new Employee("Solid", "Snake", 18, 3));
@@ -69,5 +125,7 @@ namespace LinqExercise
             return employees;
         }
         #endregion
+
+
     }
 }
